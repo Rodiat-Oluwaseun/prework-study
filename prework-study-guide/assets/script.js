b366054918bffeac93dab86531386eb7120cc6d5
Rhoddy Seun <rodiat71@gmail.com>
2023-09-26 14:18:53 +0100

    added javascript to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,18 @@
+var topic = "HTML";
+
+// if (topics === 'HTML'){
+// console.log("Lets study HTML")
+// }else if(topics ==='CSS'){
+//     console.log("Lets study CSS")
+// }else if(topics ==='Git'){
+//     console.log("Lets study GIT")
+// }else if(topics ==='Javascript'){
+//     console.log("Lets study Javascript")
+// }else{
+//     console.log("Please Try again")
+// }
+
+var topics = ['HTML','CSS','Git','Javascript'];
+for(var x = 0; x < topics.length; x++) {
+ console.log(topics[x]);
+}
